84ae0216ae4c9a074e75ea92c61695b8845c8366
Wlic <1843978072@qq.com>
2022-04-09 23:09:15 +0800

    4.9

---
+++ locker.h
@@ -0,0 +1,104 @@
+
+#ifndef WEBSERVER_LOCKER_H
+#define WEBSERVER_LOCKER_H
+
+#include <pthread.h>
+#include <exception>
+#include <semaphore.h>
+
+// 线程同步机制封装类
+
+// 互斥锁类
+class locker {
+public:
+    locker(){
+        if(pthread_mutex_init(&m_mutex, nullptr) != 0) {
+            throw std::exception();
+        }
+    }
+    // 上锁
+    bool lock() {
+        return pthread_mutex_lock(&m_mutex) == 0;
+    }
+    // 解锁
+    bool unlock() {
+        return pthread_mutex_unlock(&m_mutex) == 0;
+    }
+    // 获得锁
+    pthread_mutex_t *get() {
+        return &m_mutex;
+    }
+
+ ~locker() {
+     pthread_mutex_destroy(&m_mutex);
+    }
+private:
+    pthread_mutex_t m_mutex;
+};
+
+// 条件变量类
+class cond {
+public:
+    cond() {
+    if(pthread_cond_init(&m_cond, nullptr) != 0) {
+        throw std::exception();
+    }
+    }
+   // 等待，调用函数，线程会阻塞
+    bool wait(pthread_mutex_t &m_mutex) {
+    int ret = 0;
+    ret = pthread_cond_wait(&m_cond,&m_mutex);
+    return ret == 0;
+    }
+
+    // 等待多长时间，调用了这个函数，线程会阻塞，直到指定的时间结束
+    bool timewait(pthread_mutex_t *m_mutex,const struct timespec t) {
+        int ret = 0;
+        ret = pthread_cond_timedwait(&m_cond,m_mutex,&t);
+        return ret == 0;
+    }
+
+    // 唤醒一个或者多个等待的线程
+    bool signal() {
+        return pthread_cond_signal(&m_cond) == 0;
+    }
+
+    // 唤醒所有的等待的线程
+    bool broadcast() {
+        return pthread_cond_broadcast(&m_cond) == 0;
+    }
+
+    ~cond() {
+        pthread_cond_destroy(&m_cond);
+    }
+private:
+    pthread_cond_t m_cond;
+};
+
+// 信号量类
+class sem{
+public:
+    sem() {
+        if(sem_init(&m_sem,0,0) != 0) {
+            throw std::exception();
+        }
+    }
+
+    // 等待信号量
+    bool wait() {
+        return sem_wait(&m_sem) == 0;
+    }
+
+    // 增加信号量 给信号量的值加上一个“1”
+    bool post() {
+        return sem_post(&m_sem) == 0;
+    }
+
+    ~sem() {
+        sem_destroy(&m_sem);
+    }
+private:
+sem_t m_sem;
+};
+
+#endif //WEBSERVER_LOCKER_H
---
+++ threadpool.h
@@ -0,0 +1,114 @@
+
+#ifndef WEBSERVER_ThreadPool_H
+#define WEBSERVER_ThreadPool_H
+
+#include "locker.h"
+#include <list>
+#include <cstdio>
+
+// 线程池类，将它定义为模板类是为了代码复用
+template<typename T>
+class ThreadPool {
+public:
+    // thread_number是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量
+    ThreadPool(int thread_number = 10,int max_requests = 10000);
+
+    bool append(T* request);
+
+    ~ThreadPool();
+private:
+    // 线程的数量
+    int m_thread_num;
+
+    // 描述线程池的数组,大小为 m_thread_num
+    pthread_t* m_threads;
+
+    // 请求队列中最多允许的、等待处理的请求的数量
+    int m_max_requests;
+
+    // 请求队列
+    std::list<T*> m_work_que;
+
+    // 保护请求队列的互斥锁
+    locker m_que_locker;
+
+    // 是否有任务需要处理
+    sem m_que_stat;
+
+    // 是否结束线程
+    bool m_stop;
+
+private:
+    static void *worker(void *arg);
+    void run();
+};
+
+template<typename T>
+ThreadPool<T>::ThreadPool(int thread_number, int max_requests):
+m_thread_num(thread_number),m_max_requests(max_requests),m_threads(nullptr),m_stop(false){
+
+    if(thread_number <= 0 || max_requests <= 0) {
+        throw std::exception();
+    }
+
+    m_threads = new pthread_t[m_thread_num];
+    if(!m_threads) {
+        throw std::exception();
+    }
+
+    // 创建thread_number 个线程，并将他们设置为脱离线程
+    for(int i = 0; i < thread_number; ++i) {
+        printf("create the %dth thread\n",i);
+        if(pthread_create(m_threads+i, nullptr,worker,this) != 0) {
+            delete[] m_threads;
+            throw std::exception();
+        }
+        // 线程分离
+        if(pthread_detach(m_threads[i])) {
+            delete[] m_threads;
+            throw std::exception();
+        }
+    }
+}
+
+template<typename T>
+ThreadPool<T>::~ThreadPool<T>() {
+    delete[] m_threads;
+    this->m_stop = true;
+}
+
+template<typename T>
+bool ThreadPool<T>::append(T *request) {
+    // 操作工作队列时要加锁，因为它被所有线程共享
+    m_que_locker.lock();
+    // 超过的允许的最大请求数
+    if(m_work_que.size() >= m_max_requests) {
+        m_que_locker.unlock();
+        return false;
+    }
+    m_work_que.push_back(request);
+    m_que_locker.unlock();
+    m_que_stat.post(); // 记得信号量加 1
+    return true;
+}
+
+template< typename T >
+void *ThreadPool<T>::worker(void *arg) {
+    ThreadPool * pool = (ThreadPool *)arg;
+    pool->run();
+    return pool;
+}
+
+template< typename T >
+void ThreadPool<T>::run() {
+    while(!m_stop) {
+        m_que_stat.wait();  // 信号量 -1 没有数据时阻塞
+        m_que_locker.lock();
+        T* request = m_work_que.front(); // 取出第一个
+        m_work_que.pop_front();
+        m_que_locker.unlock();
+        request->process;
+    }
+}
+
+#endif //WEBSERVER_ThreadPool_H
